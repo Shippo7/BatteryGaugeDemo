5fe10daaeddcfaf118d002e33f8ca46b7a4efa2f
XueFei Yang <shippo.yxf@gmail.com>
2015-06-15 22:05:02 -0400

    Initial Commit

---
+++ BatteryGaugeDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BatteryGaugeDemo
+//
+//  Created by XueFei Yang on 2015-06-15.
+//  Copyright (c) 2015 XueFei Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
